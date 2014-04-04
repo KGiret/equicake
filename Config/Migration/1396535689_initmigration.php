9c40c27a24ce0fd50a6751a64da11deeb55e45f6
KGiret <kevin.giret@y-nov.com>
2014-04-04 11:26:22 +0200

    initialcommit

---
+++ Config/Migration/1396535689_initmigration.php
@@ -0,0 +1,157 @@
+<?php
+class Initmigration extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'articles' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'title' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 25, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'contents' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 2000, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'date' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'screenshot_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'video_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'tier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 2, 'key' => 'index'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'screenshot_id' => array('column' => 'screenshot_id', 'unique' => 0),
+						'video_id' => array('column' => 'video_id', 'unique' => 0),
+						'tier_id' => array('column' => 'tier_id', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'classes' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'players' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'classe_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'specialite_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'role_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'rank_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'classe_id' => array('column' => 'classe_id', 'unique' => 0),
+						'spe_id' => array('column' => 'specialite_id', 'unique' => 0),
+						'role_id' => array('column' => 'role_id', 'unique' => 0),
+						'rang_id' => array('column' => 'rank_id', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'ranks' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 11, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'roles' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 10, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'screenshots' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 25, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'tier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 2, 'key' => 'index'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'tier_id' => array('column' => 'tier_id', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'settings' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'contents' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 5000, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'specialties' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'nom' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 30, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'state' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+					'classe_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'key' => 'index'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'classe_id' => array('column' => 'classe_id', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'tiers' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'number' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 2),
+					'state' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 1),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+				'videos' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+					'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 25, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'link' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+					'tier_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 2, 'key' => 'index'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'tier_id' => array('column' => 'tier_id', 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'articles', 'classes', 'players', 'ranks', 'roles', 'screenshots', 'settings', 'specialties', 'tiers', 'videos'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
