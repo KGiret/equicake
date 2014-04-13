6bf0917f00d14095c6381716f735f953628c5d11
mathieubourgeois <math.bourgeois@orange.fr>
2014-04-13 15:01:23 +0200

    Create controller Videos

---
+++ Controller/VideosController.php
@@ -0,0 +1,100 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Videos Controller
+ *
+ * @property Video $Video
+ * @property PaginatorComponent $Paginator
+ */
+class VideosController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Video->recursive = 0;
+		$this->set('videos', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Video->exists($id)) {
+			throw new NotFoundException(__('Invalid video'));
+		}
+		$options = array('conditions' => array('Video.' . $this->Video->primaryKey => $id));
+		$this->set('video', $this->Video->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Video->create();
+			if ($this->Video->save($this->request->data)) {
+				return $this->flash(__('The video has been saved.'), array('action' => 'index'));
+			}
+		}
+		$tiers = $this->Video->Tier->find('list');
+		$this->set(compact('tiers'));
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Video->exists($id)) {
+			throw new NotFoundException(__('Invalid video'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Video->save($this->request->data)) {
+				return $this->flash(__('The video has been saved.'), array('action' => 'index'));
+			}
+		} else {
+			$options = array('conditions' => array('Video.' . $this->Video->primaryKey => $id));
+			$this->request->data = $this->Video->find('first', $options);
+		}
+		$tiers = $this->Video->Tier->find('list');
+		$this->set(compact('tiers'));
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Video->id = $id;
+		if (!$this->Video->exists()) {
+			throw new NotFoundException(__('Invalid video'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Video->delete()) {
+			return $this->flash(__('The video has been deleted.'), array('action' => 'index'));
+		} else {
+			return $this->flash(__('The video could not be deleted. Please, try again.'), array('action' => 'index'));
+		}
+	}}
