201e003d43427bb4da2a51b27add0140b0160777
KGiret <kevin.giret@y-nov.com>
2014-05-09 15:36:19 +0200

    add before save function in player model

--- Model/Player.php
+++ Model/Player.php
@@ -112,4 +112,11 @@ class Player extends AppModel {
 			'order' => ''
 		)
 	);
+
+		public function beforeSave($options = array())
+	{
+		$this->request->data['name'] = htmlentities($this->request->data['name']);
+
+		return true;
+	}
 }
