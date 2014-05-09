42d52ce872f5a8ad171ba357d3eba131e910b8ab
KGiret <kevin.giret@y-nov.com>
2014-05-09 16:50:38 +0200

    Config routing option

--- Config/routes.php
+++ Config/routes.php
@@ -11,7 +11,15 @@ Router::connect('/opauth-complete/*',		array('controller' => 'authentications', 
 /**
  * App Routing
  */
-Router::connect('/:controller',				array('action' => 'index'));
+/*Router::connect('/:controller',				array('action' => 'index'));
 Router::connect('/:controller/:id',			array('action' => 'view'), array('pass' => array('id'), 'id' => '[0-9]+'));
 Router::connect('/:controller/:action/:id',	array(), array('pass' => array('id'), 'id' => '[0-9]+'));
-Router::connect('/:controller/:action',		array());
+Router::connect('/:controller/:action',		array());*/
+
+/**
+ * EquinoXx routing
+ */
+Router::connect('/accueil', 				array('controller' => 'articles', 'action' => 'index'));
+Router::connect('/roster', 				array('controller' => 'players', 'action' => 'index'));
+Router::connect('/media', 				array('controller' => 'screenshots', 'action' => 'index'));
+Router::connect('/archives', 				array('controller' => 'articles', 'action' => 'index'));
